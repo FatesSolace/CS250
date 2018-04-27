d0367e7e57281c1117538ae20eb1cc8f2d1973bd
Becca Williams <38543055+FatesSolace@users.noreply.github.com>
2018-04-26 22:00:01 -0500

    Lab 8 - Templates

---
+++ lab8_arraywrapper.hpp
@@ -0,0 +1,38 @@
+#ifndef _ARRAY_WRAPPER
+#define _ARRAY_WRAPPER
+
+const int MAX_SIZE = 10;
+template <typename T>
+
+class ArrayWrapper
+{
+	ArrayWrapper()
+	{
+		m_itemCount = 0;
+	}
+
+	void Push(T data)
+	{
+		if (m_itemCount < MAX_SIZE)
+		{
+			m_data[m_itemCount] = data;
+			m_itemCount++;
+		}
+	}
+
+	void Display()
+	{
+		for (int i = 0; i < MAX_SIZE; i++)
+		{
+			cout << m_data[i];
+		}
+	}
+
+private:
+	T m_data[MAX_SIZE];
+	int m_itemCount;
+};
+
+
+
+#endif
